d4124b003d37bcf6678aa6253cdd6f6740edb1ae
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2023-05-28 08:44:11 -0500

    Update load-theme.test.js

--- test/load-theme.test.js
+++ test/load-theme.test.js
@@ -3,16 +3,12 @@ import { assertEquals, assertRejects } from 'asserts'
 import { loadTheme } from '../index.js'
 
 Deno.test('loadTheme should parse the JSON file', async () => {
-  const expected = {
-    message: 'Hello, World!'
-  }
+  const expected = { message: 'Hello, World!' }
   const actual = await loadTheme('./test/test.json')
   assertEquals(actual, expected)
 })
 
 Deno.test('loadTheme should throw an error if the file is not found', async () => {
-  const testFunction = async () => {
-    await loadTheme('./test/non-existent.json')
-  }
+  const testFunction = () => loadTheme('./test/non-existent.json')
   await assertRejects(testFunction, Deno.errors.NotFound, 'No such file or directory')
 })
