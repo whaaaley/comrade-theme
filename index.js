ba77e016e7ee2afbcb5dbc5733ab426593baef2d
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2023-05-29 06:30:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ export const getColorLocations = (input, placeholder = '') => {
 
     visited.add(input)
 
-    for (let [key, value] of Object.entries(input)) {
-      let newPrefix = prefix ? `${prefix}.${key}` : key
+    for (const [key, value] of Object.entries(input)) {
+      const newPrefix = prefix ? `${prefix}.${key}` : key
 
       if (!Array.isArray(value)) {
         if (typeof value === 'object') { // Ignore arrays
@@ -52,7 +52,7 @@ export const getColorLocations = (input, placeholder = '') => {
             result[newPrefix] = result[newPrefix] || placeholder
           }
         } else if (colord(value).isValid()) { // Check if value is a valid color
-          let baseKey = newPrefix.split('.')[0]
+          const baseKey = newPrefix.split('.')[0]
 
           result[baseKey] = result[baseKey] || {}
           result[baseKey][value] = result[baseKey][value] || []
@@ -68,47 +68,160 @@ export const getColorLocations = (input, placeholder = '') => {
 }
 
 export const nearestColor = (input, arr) => {
-  let result = { color: '', delta: Infinity }
+  let result = ''
+  let minDelta = Infinity
 
   for (const color of arr) {
     const delta = colord(input).delta(color)
 
-    if (delta < result.delta) {
-      result = { color, delta }
+    if (delta < minDelta) {
+      result = color
+      minDelta = delta
     }
   }
 
   return result
 }
 
+export const mergeSubThemes = (primary, secondary) => {
+  const result = {}
+  const secondaryColors = Object.keys(secondary)
+
+  for (const color in primary) {
+    const nearest = nearestColor(color, secondaryColors)
+    const averagedColor = colord(nearest).mix(color).toHex().toUpperCase()
+
+    if (result[averagedColor] == null) {
+      result[averagedColor] = primary[color]
+    }
+  }
+
+  return result
+}
+
+export const mergeThemes = (primary, secondary) => {
+  const result = {}
+
+  for (const [key, value] of Object.entries(primary)) {
+    const primarySubTheme = primary[key]
+    const secondarySubTheme = secondary[key]
+
+    if (primary.hasOwnProperty(key)) {
+      primary[key] = mergeSubThemes(primary[key], value)
+    } else {
+      primary[key] = value
+    }
+  }
+
+  return primary
+}
+
+// Theme popularity
+// + GitHub Theme
+// + Atom One Dark
+// + Material Community
+// + Solarized Dark
+export const director = async () => {
+  const githubPromise = loadTheme('./themes/github-dimmed.json')
+  const oneDarkPromise = loadTheme('./themes/one-dark-pro.json')
+  const materialPromise = loadTheme('./themes/community-material-theme-high-contrast.json')
+  const solarizedPromise = loadTheme('./themes/solarized-dark.json')
+
+  const github = await githubPromise
+  const oneDark = await oneDarkPromise
+  const material = await materialPromise
+  const solarized = await solarizedPromise
+
+  const result = mergeThemes(
+    mergeThemes(github, oneDark),
+    mergeThemes(material, solarized)
+  )
+
+  console.log(JSON.stringify(result, null, 2))
+
+  return result
+}
+
+// const isEqual = (set1, set2) => {
+//   if (set1.size !== set2.size) {
+//     return false
+//   }
+//
+//   for (const element of set1) {
+//     if (!set2.has(element)) {
+//       return false
+//     }
+//   }
+//
+//   return true
+// }
+
+// export const mergeSubThemes = (subTheme1, subTheme2) => {
+//   const result = { ...subTheme1 }
+//
+//   for (const color2 in subTheme2) {
+//     const selectors2 = new Set(subTheme2[color2])
+//
+//     if (result.hasOwnProperty(color2)) {
+//       // If the color already exists in the result theme, merge the selectors.
+//       const selectors1 = new Set(result[color2])
+//       result[color2] = [...new Set([...selectors1, ...selectors2])]
+//     } else {
+//       // If the color doesn't exist in the result theme, find the nearest color from result.
+//       const nearest = nearestColor(color2, Object.keys(result))
+//
+//       // Average the nearest color with the current color, replace the nearest color in result with the new averaged color,
+//       // and assign it the selectors of the nearest color.
+//       const averagedColor = colord(nearest.color).mix(color2).toHex().toUpperCase()
+//       result[averagedColor] = result[nearest.color]
+//
+//       // Delete the old color key from the result theme.
+//       delete result[nearest.color]
+//     }
+//   }
+//
+//   console.log('result:', result)
+//
+//   return result
+// }
+
+// export const mergeSubThemes = (subTheme1, subTheme2) => {
+//   const mergedSubTheme = { ...subTheme1 }
+//   const updates = []
+//
+//   for (const color2 in subTheme2) {
+//     const nearest = nearestColor(color2, Object.keys(mergedSubTheme))
+//     const averagedColor = colord(nearest.color).mix(color2).toHex()
+//     const selectors = [...new Set((mergedSubTheme[nearest.color] || []).concat(subTheme2[color2]))]
+//
+//     console.log('nearest:', nearest)
+//     console.log('averagedColor:', averagedColor)
+//     console.log('selectors:', selectors)
+//
+//     updates.push({
+//       oldColor: nearest.color,
+//       newColor: averagedColor,
+//       selectors: selectors
+//     })
+//   }
+//
+//   console.log('updates:', updates)
+//
+//   for (const update of updates) {
+//     delete mergedSubTheme[update.oldColor]
+//     mergedSubTheme[update.newColor] = update.selectors
+//   }
+//
+//   console.log('mergedSubTheme:', mergedSubTheme)
+//
+//   return mergedSubTheme
+// }
+
 export const mergeThemes = (theme1, theme2) => {
   const mergedTheme = {}
 
-  // Merge keys from theme1
-  for (const key in theme1) {
-    const colorMap1 = theme1[key]
-    const colorMap2 = theme2[key] || {}
-    const mergedColorMap = {}
-
-    // Merge color maps from theme1 and theme2
-    for (const color in colorMap1) {
-      const selectors1 = colorMap1[color]
-      const selectors2 = colorMap2[color] || []
-      const mergedSelectors = [...new Set([...selectors1, ...selectors2])]
-
-      // Merge selectors for the same color
-      mergedColorMap[color] = mergedSelectors
-    }
-
-    // Add merged color map to merged theme
-    mergedTheme[key] = mergedColorMap
-  }
-
-  // Merge keys from theme2 that are not in theme1
-  for (const key in theme2) {
-    if (!theme1.hasOwnProperty(key)) {
-      mergedTheme[key] = theme2[key]
-    }
+  for (const key of new Set([...Object.keys(theme1), ...Object.keys(theme2)])) {
+    mergedTheme[key] = mergeSubThemes(theme1[key] || {}, theme2[key] || {})
   }
 
   return mergedTheme
